e7e901d402b05bd18a7ae17639ae79438160c80b
Stepan Mironov <stephmir@bk.ru>
2024-04-20 02:41:35 +0300

    Initial commit

--- app/src/main/java/com/example/pr4/MainActivity.java
+++ app/src/main/java/com/example/pr4/MainActivity.java
@@ -1,20 +1,9 @@
 package com.example.pr4;
 
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.pr4.databinding.ActivityMainBinding;
-
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     @Override
