eaeab49161de61b81a937ca64d606d3a00b6295d
Stepan Mironov <stephmir@bk.ru>
2024-04-20 02:40:32 +0300

    Initial commit

--- app/src/main/java/com/example/pr4/MainActivity.java
+++ app/src/main/java/com/example/pr4/MainActivity.java
@@ -11,63 +11,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.pr4.databinding.ActivityMainBinding;
 
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
 public class MainActivity extends AppCompatActivity {
-
-    static final String CHANNEL_ID = "my_channel";
-    static final int NOTIFICATION_ID = 123;
-    ActivityMainBinding binding;
-    private static final String TAG = "ACTIVITY_LIFECYCLE";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        NotificationChannel channel = new NotificationChannel(
-                CHANNEL_ID,
-                CHANNEL_ID,
-                NotificationManager.IMPORTANCE_DEFAULT);
-        channel.setDescription("");
-
-        NotificationManager notificationManager = getSystemService(NotificationManager.class);
-        notificationManager.createNotificationChannel(channel);
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show();
-        Log.i(TAG, "onStart");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show();
-        Log.i(TAG, "onResume");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show();
-        Log.i(TAG, "onPause");
-
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show();
-        Log.i(TAG, "onStop");
-    }
-
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show();
-        Log.i(TAG, "onDestroy");
+        setContentView(R.layout.activity_main);
     }
 }
